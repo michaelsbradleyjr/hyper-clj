c98879b40a1155e8124a5def0ea7c35f431256cf
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-06-09 22:09:18 -0500

    licenses/ and the first Generate scaffolding for a new project based on a template.
    If only one argument is passed, the default template is used and the
    argument is treated as if it were the name of the project.
    
    Use ":show" instead of a project name to show template details.
    
    Subtasks available:
    default    A general project template.
    plugin     A leiningen plugin project template.
    template   A meta-template for 'lein new' templates.
    
    Arguments: ([project-name] [template project-name & args]) w/ some deps spec'd

---
+++ maze/project.clj
@@ -0,0 +1,10 @@
+(defproject maze "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[compojure "1.1.0"]
+                 [hiccup "1.0.0"]
+                 [org.clojure/clojure "1.4.0"]
+                 [ring "1.1.0"]]
+  :plugins [[lein-swank "1.4.4"]])
---
+++ maze/src/maze/core.clj
@@ -0,0 +1,6 @@
+(ns maze.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
